188517d6eefa2b32f9d2d9d8c6b7d5922c3e68f7
XiaotianGuan <155344896+XiaotianGuan@users.noreply.github.com>
2024-03-12 11:30:27 +0800

    Add files via upload

--- 2in66b_pr_test.py
+++ 2in66b_pr_test.py
@@ -1,27 +1,28 @@
-# *************************************************************************************************
-# File        :  2in66b_pr_test.py
-# Version     :  v1.0.1
-# Date        :  2024-01-01
-# Author      :  GXT
-# Description :  Waveshare 2.66inch 3-color e-paper driver with limited partial refresh support
-#                Set operating mode by calling ColorMode()
-#                3-color mode is the default and standard mode of operation
-#                In 3-color mode, only global refresh is supported
-#                Only updating the black image while retaining the red image should be possible,
-#                but currently is not implemented
-#                In 2-color mode, partial refresh is available
-#                You can still write to red buffer, but red won't be displayed
-#                To enable partial refresh, call RefreshMode('partial')
-# Disclaimer :   Partial refresh is not officially supported by the display OEM
-#                Using partial refresh may cause irreversible damage to the display
-#                Use this code at your own risk
-# *************************************************************************************************
+# *******************************************************************************************************************************
+# File        : 2in66b_pr_test.py
+# Version     : v1.1.2
+# Date        : 2024-03-12
+# Author      : GXT
+# Description : Waveshare 2.66inch 3-color e-paper driver with limited partial refresh support
+#               Set operating mode by calling ColorMode(), default is '3-color' mode
+#               In '3-color' mode, only global refresh is supported
+#               Only updating the black image while retaining the red image should be possible, but currently is not implemented
+#               In '2-color' mode, partial refresh is available
+#               Set whether content in red buffer is displayed in black by calling CombineRB()
+#               To enable partial refresh, call RefreshMode('partial')
+# Disclaimer  : Partial refresh is not officially supported by the display OEM, may cause irreversible damage to the display
+#               Use this code at your own risk
+# Known issues: ·Ensuring no red is on the display in '2-color' mode is not enforced in code, this duty falls on the user
+#                Calling partial refresh/clear in '2-color' mode with red on the display may lead to unexpected behaviors
+#               ·In 2-color mode, when using global refresh, calling Refresh()/Draw() will use the custom LUT instead of the
+#                display's factory LUT. Except in auto refresh, the factory LUT is used
+# *******************************************************************************************************************************
 
 
 from machine import Pin, SPI
 import framebuf
 import utime
-import neopixel
+
 
 class EPD_2in66_B:
     _x_res = 152
@@ -82,9 +83,10 @@ class EPD_2in66_B:
         self.ColorMode(self._color_mode)
         self._refresh_mode = REFRESHMODE
         self.RefreshMode(self._refresh_mode)
+        self._CRB = False
         self._max_pr = 0
         self._pr = 0
-        self._fr_enabled = False
+        self._ar_enabled = False
         self.black_buffer.fill(1)
         self.red_buffer.fill(1)
     
@@ -99,7 +101,7 @@ class EPD_2in66_B:
         self._cs_pin.low()
         self._spi.write(bytearray([data]))
         self._cs_pin.high()
-        
+    
     def __SendLUT(self):
         self.__SendCommand(0x32)
         for i in range(0, 153):
@@ -138,6 +140,22 @@ class EPD_2in66_B:
                 for i in range(0, self._y_bit):
                     self.__SendData(~self.__ReverseByte(self._red_buffer_array[(j + 1) * self._y_bit - 1 - i]))
     
+    def __SendRB(self):
+        if (self._orientation == 'portrait'):
+            for i in range(0, self._x_byte * self._y_bit):
+                self.__SendData(self._black_buffer_array[i] & self._red_buffer_array[i])
+        elif (self._orientation == 'portrait_flipped'):
+            for i in range(0, self._x_byte * self._y_bit):
+                self.__SendData(self.__ReverseByte(self._black_buffer_array[self._x_byte * self._y_bit - 1 - i] & self._red_buffer_array[self._x_byte * self._y_bit - 1 - i]))
+        elif (self._orientation == 'landscape'):
+            for j in range(0, self._x_byte):
+                for i in range(0, self._y_bit):
+                    self.__SendData(self._black_buffer_array[i + (self._x_byte - j - 1) * self._y_bit] & self._red_buffer_array[i + (self._x_byte - j - 1) * self._y_bit])
+        else:
+            for j in range(0, self._x_byte):
+                for i in range(0, self._y_bit):
+                    self.__SendData(self.__ReverseByte(self._black_buffer_array[(j + 1) * self._y_bit - 1 - i] & self._red_buffer_array[(j + 1) * self._y_bit - 1 - i]))
+    
     def __ReadBusy(self):
         while(self._busy_pin.value() == 1):
             self.__Delayms(50)
@@ -171,14 +189,20 @@ class EPD_2in66_B:
         self._reset_pin.high()
         self.__Delayms(50) 
     
-    def ColorMode(self, COLORMODE):
+    # always call RefreshMode() after changing color mode, even if refresh mode is not changed
+    # when switching from '3-color' mode to '2-color' mode, if there's red on the display, set REFRESH = True
+    def ColorMode(self, COLORMODE, REFRESH = False):
         if COLORMODE == '3-color':
+            self._color_mode = COLORMODE
             print('3-color mode')
         elif COLORMODE == '2-color':
-            print('2-color mode. You can still write to red buffer, but red will not be displayed')
+            self._color_mode = COLORMODE
+            print('2-color mode')
+            if REFRESH:
+                self.RefreshMode('global')
+                self.Draw()
         else:
             raise ValueError('\'COLORMODE\' in ColorMode()')
-        self._color_mode = COLORMODE
     
     def RefreshMode(self, REFRESHMODE):
         self.Reset()
@@ -237,16 +261,9 @@ class EPD_2in66_B:
                 self._refresh_mode = REFRESHMODE
             else:
                 raise ValueError('\'REFRESHMODE\' in RefreshMode()')
-            
-            
-            
-            
-
-
     
-    
-    
-    
+    # manual refresh, will display the epd's internal buffer
+    # any changes made to the black_buffer/red_buffer after last Draw() command will not be displayed
     def Refresh(self):
         print('Refresh')
         if self._refresh_mode == 'global':
@@ -256,28 +273,56 @@ class EPD_2in66_B:
             self.Draw()
             self.RefreshMode('partial')
     
-    def ForceRefresh(self, FR = False, MAX_PR = 99):
-        self._fr_enabled = FR
+    # set maximum interval between two global refresh when refresh mode is 'partial'
+    # has no effect in '3-color' mode
+    def AutoRefresh(self, AR = False, MAX_PR = 9):
+        self._ar_enabled = AR
         self._max_pr = MAX_PR
     
-    
-
-    
-    
+    # CombineRB(True): in '2-color' mode, content in red buffer will be displayed in black
+    # CombineRB(False): in '2-color' mode, content in red buffer will not be displayed
+    # has no effect in '3-color' mode
+    def CombineRB(self, CRB):
+        self._CRB = CRB
     
     def Draw(self):
         print('Drawing')
-        self.__SendCommand(0x24)
-        self.__SendBlack()
         if self._color_mode == '3-color':
-            if self._refresh_mode == 'global':
-                self.__SendCommand(0x26)
-                self.__SendRed()
-            else:
-                raise NotImplementedError('Partial refresh is not implemented for 3-color mode')
+            self.__SendCommand(0x24)
+            self.__SendBlack()
+            self.__SendCommand(0x26)
+            self.__SendRed()
+            self.__TurnOnDisplay()
+            self._pr = 0
         else:
-            pass
-        self.__TurnOnDisplay()
+            if self._ar_enabled and self._refresh_mode == 'partial' and self._pr >= self._max_pr:
+                self.ColorMode('3-color')	# refresh using the factory LUT
+                self.RefreshMode('global')
+                self.__SendCommand(0x26)	# manually send red(all 0), so red image is kept in buffer
+                for i in range(0, self._x_byte * self._y_bit):
+                    self.__SendData(0x00)
+                if self._CRB:
+                    self.__SendCommand(0x24)
+                    self.__SendRB()
+                else:
+                    self.__SendCommand(0x24)
+                    self.__SendBlack()
+                self.__TurnOnDisplay()
+                self.ColorMode('2-color')
+                self.RefreshMode('partial')
+                self._pr = 0
+            else:
+                if self._CRB:
+                    self.__SendCommand(0x24)
+                    self.__SendRB()
+                else:
+                    self.__SendCommand(0x24)
+                    self.__SendBlack()
+                self.__TurnOnDisplay()
+                if self._ar_enabled and self._refresh_mode == 'partial':
+                    self._pr += 1
+                else:
+                    self._pr = 0
         print('Drawn')
     
     def Clear(self, MODE = 'global'):
@@ -309,20 +354,15 @@ class EPD_2in66_B:
         self.__SendData(0x01)
         print("Sleep")
 
+
 if __name__=='__main__':
-    RGB = neopixel.NeoPixel(machine.Pin(16), 1)
-    RGB.fill((128, 0, 0))
-    RGB.write()
     RST = Pin(7, Pin.OUT)
     DC = Pin(8, Pin.OUT)
     CS = Pin(9, Pin.OUT)
     BUSY = Pin(6, Pin.IN, Pin.PULL_UP)
     epd_spi = SPI(1, baudrate = 4000000, polarity = 0, phase = 0, bits = 8, firstbit = SPI.MSB, sck = Pin(10), mosi = Pin(11), miso = Pin(12))
     epd = EPD_2in66_B(RST, DC, CS, BUSY, epd_spi, 'landscape_flipped')
-    epd.Clear()
     
-    RGB.fill((0, 128, 0))
-    RGB.write()
     epd.ColorMode('2-color')
     epd.RefreshMode('partial')
     epd.black_buffer.rect(0, 0, 4, 16, 0, True)
@@ -340,15 +380,11 @@ if __name__=='__main__':
     epd.Draw()
     epd.black_buffer.text("296x152", 98, 40, 0)
     epd.Draw()
-    epd.RefreshMode('global')
     epd.black_buffer.text("Firmware:", 10, 55, 0)
     epd.Draw()
-    epd.RefreshMode('partial')
     epd.black_buffer.text("2in66b_pr_test.py", 82, 55, 0)
     epd.Draw()
     
-    RGB.fill((0, 0, 128))
-    RGB.write()
     epd.ColorMode('3-color')
     epd.RefreshMode('global')
     epd.red_buffer.vline(10, 90, 40, 0)
@@ -357,21 +393,24 @@ if __name__=='__main__':
     epd.black_buffer.hline(10, 130, 80, 0)
     epd.red_buffer.line(10, 90, 90, 130, 0)
     epd.black_buffer.line(90, 90, 10, 130, 0)
-    epd.Draw()
-    epd.red_buffer.fill(1)
     epd.red_buffer.vline(10, 90, 40, 0)
     epd.red_buffer.vline(90, 90, 40, 0)
     epd.red_buffer.line(10, 90, 90, 130, 0)
     epd.black_buffer.rect(120, 90, 40, 40, 0, True)
-    epd.Draw()
     epd.red_buffer.rect(190, 90, 40, 40, 0, True)
     epd.Draw()
     
-    RGB.fill((64, 64, 0))
-    RGB.write()
+    epd.CombineRB(True)
+    epd.ColorMode('2-color', REFRESH = True)
+    epd.RefreshMode('partial')
+    for i in range(10):
+        epd.black_buffer.fill_rect(250, 100, 20, 20, 1)
+        epd.black_buffer.text(str(i), 256, 106, 0)
+        epd.Draw()
+    
+    epd.ColorMode('3-color')
+    epd.RefreshMode('global')
+    epd.black_buffer.fill_rect(250, 100, 20, 20, 1)
+    epd.Draw()
     epd.Clear()
-    
-    RGB.fill((0, 0, 0))
-    RGB.write()
-    
     epd.Sleep()
